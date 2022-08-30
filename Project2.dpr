2048bdfbf1cd14226b4a5a531cc550da1fc8a17e
Miguel Anacleto Ricken <miguelricken123@hotmail.com>
2022-08-29 22:41:15 -0300

    segunod ta dado

---
+++ Project2.dpr
@@ -0,0 +1,16 @@
+program Project2;
+
+uses
+  Vcl.Forms,
+  Tela_principal in 'Tela_principal.pas' {Form1},
+  Unit2 in 'Unit2.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
